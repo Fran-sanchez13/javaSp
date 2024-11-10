43fce49e685f68c56ac7912603a1d4d3167d0727
francisco-sanchez13 <franciscosanchezp44@gmail.com>
2024-11-10 17:50:12 -0300

    mostrar img en contenedor input

--- index.js
+++ index.js
@@ -48,3 +48,32 @@ const pizzas = [
 
 // ENTREGA NRO 3 NUCBA
 
+// document.getElementById('pizza'). addEventListener('submit', (e)){
+//   e.preventDefault();
+
+//   const input = document.getElementById('numberInput').value;
+//   const results = document.getElementById('resultsPizza');
+
+//   if(input){
+//     results.textContent = `La pizza que buscas es: ${input}`;
+//   }
+//   else{
+//     results.textContent = 'Por favor, ingrese un numero valido',
+//   }
+
+
+
+// }
+
+document.getElementById('pizzaForm').addEventListener('submit', function(event) {
+  event.preventDefault(); // Evita que el formulario se envíe y recargue la página
+  
+  const input = document.getElementById('numberInput').value;
+  const resultado = document.getElementById('resultsPizza');
+
+  if (input) {
+      resultado.textContent = `Resultado de la búsqueda para el número: ${input}`;
+  } else {
+      resultado.textContent = 'Por favor, introduce un número válido.';
+  }
+});
